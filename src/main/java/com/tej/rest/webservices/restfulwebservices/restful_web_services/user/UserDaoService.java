bd1a6b9fd9f44c84f1f4fca1f55e0873526644ce
tejpalla <pallatej7@gmail.com>
2025-04-24 20:01:14 +0530

    third commit

--- src/main/java/com/tej/rest/webservices/restfulwebservices/restful_web_services/user/UserDaoService.java
+++ src/main/java/com/tej/rest/webservices/restfulwebservices/restful_web_services/user/UserDaoService.java
@@ -25,11 +25,12 @@ public class UserDaoService {
     
     public User findOne(int id){
         Predicate<? super User> predicate = user -> user.getId() == id; 
-        return users.stream().filter(predicate).findFirst().get();
+        return users.stream().filter(predicate).findFirst().orElse(null);
     }
 
-    public void save(User user){
+    public User save(User user){
         user.setId(++usrcout);
         users.add(user);
+        return user;
     }
 }
